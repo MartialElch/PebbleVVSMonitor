0f6d547181e545ea56451e20e5a9bb7b9cd0af3f
MartialElch <stefan.reimbold@imail.de>
2015-05-07 20:35:13 +0200

    add some comments
    add comments and format code

--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
+// fetch list of train numbers, destination and time to departure from VVS server
 function getDepartures(stopID) {
-	var req = new XMLHttpRequest();
-	var url = 'http://www2.vvs.de/vvs/XML_DM_REQUEST?outputFormat=JSON&sessionID=0&depType=stopEvents&mode=direct&limit=10&mergeDep=1&useRealtime=1&line=any&type_dm=stationID&name_dm='+ stopID;
+  var req = new XMLHttpRequest();
+  var url = 'http://www2.vvs.de/vvs/XML_DM_REQUEST?outputFormat=JSON&sessionID=0&depType=stopEvents&mode=direct&limit=10&mergeDep=1&useRealtime=1&line=any&type_dm=stationID&name_dm='+ stopID;
   var response;
 
   req.open('GET', url, true);
 
-	req.onload = function(e) {
-		if (req.status == 200) {
-			response = JSON.parse(req.responseText);
+  req.onload = function(e) {
+    if (req.status == 200) {
+      response = JSON.parse(req.responseText);
 
-			var num = 0;
-			num = response.departureList.length;
+      var num = 0;
+      num = response.departureList.length;
 
       var name = "";
       var countdown = [];
@@ -39,15 +40,16 @@ function getDepartures(stopID) {
           console.log('Error sending departure info to Pebble!');
         }
       );
-		}
-	};
+    }
+  };
 
-	req.onerror = function(e) {
-	};
+  req.onerror = function(e) {
+  };
 
-	req.send(null);
+  req.send(null);
 }
 
+// get closest station by GPS position
 function getStationsGPS(position) {
   var MAX_STOPS = 1;
   var req = new XMLHttpRequest();
@@ -56,19 +58,20 @@ function getStationsGPS(position) {
 
   var response;
 
+  // this is test code and needs to be removed, before release
   console.log('DEBUG: lat = ' + lat);
   console.log('DEBUG: lon = ' + lon);
-
   lat = 48.775028;
   lon = 9.171567;
-  
-  var url = 'http://www2.vvs.de/vvs/XML_COORD_REQUEST?outputFormat=JSON&coordOutputFormat=WGS84&max=' + MAX_STOPS + '&inclFilter=1&radius_1=1320&type_1=STOP&coord=' + lon + ':' + lat + ':WGS84';
-	req.open('GET', url, true);
 
-	req.onload = function(e) {
+  // get closest station
+  var url = 'http://www2.vvs.de/vvs/XML_COORD_REQUEST?outputFormat=JSON&coordOutputFormat=WGS84&max=' + MAX_STOPS + '&inclFilter=1&radius_1=1320&type_1=STOP&coord=' + lon + ':' + lat + ':WGS84';
+  req.open('GET', url, true);
+
+  req.onload = function(e) {
     console.log('DEBUG: received stations');
-		if (req.status == 200) {
-			response = JSON.parse(req.responseText);
+    if (req.status == 200) {
+      response = JSON.parse(req.responseText);
 
       var message = {
         "KEY_UPDATE_STATION": 1,
@@ -88,18 +91,24 @@ function getStationsGPS(position) {
     }
   };
 
-	req.onerror = function(e) {
-	};
+  req.onerror = function(e) {
+  };
 
-	req.send(null);
-
+  req.send(null);
 }
 
+// get GPS coordinates of current location
 function getLocation() {
   navigator.geolocation.getCurrentPosition(getStationsGPS);
 }
 
 // listen for AppMessage from Pebble
+// Pebble requests an update by message
+// This function does:
+// - get the closest station by using GPS position
+// - fetch a list of departures with countdown in minutes from VVS
+// - send the list of train numbers, destination name and countdown
+//   together with the station name to Pebble
 Pebble.addEventListener("appmessage", function(e) {
   console.log('DEBUG: AppMessage received ' + JSON.stringify (e.payload));
 
